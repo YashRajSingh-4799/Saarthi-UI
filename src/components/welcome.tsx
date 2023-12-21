b36d31ffba5465b83b58aa77d35aefa9831ba629
Yash Raj Singh <yashrajsingh4799@gmail.com>
2023-12-21 15:26:13 +0530

    Welcome Screen UI Implemented

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,56 +1,60 @@
 import sarthilogo from "../assets/sarthi_logo.svg";
+import seek from "../assets/seek.svg";
+import journey from "../assets/journey.svg";
+import story from "../assets/story.svg";
 export default function OTPLogin() {
   return (
     <div className="w-[414px] h-screen">
       <div className="text-center font-extrabold text-black w-[414px] h-screen top-0 left-0 [background:linear-gradient(180deg,rgb(0,100,155)_10%,rgb(247,246,242)_22.05%,rgb(152,185,204)_71.88%,rgb(0,39,78)_100%)] ">
-        <div className="flex text-4xl pt-20">
-          It appears you have not reached out to Sarathi before.
+        <div className="flex justify-center pt-12">
+          <img src={sarthilogo} className="w-24" alt="" />
+        </div>
+
+        <div className="flex text-3xl">
+          Welcome! <br />I will be your Sarathi in your awesome journey of life
+          to true happiness.
         </div>
 
         <div className="text-center text-sm text-white mt-10 font-extralight">
-          Please enter the OTP shared on <br />
-          <span className="text-green-600">
-            +91 9978656787
-            <span className="text-white"> and </span>
-          </span>
-          to get in.
+          Going forward you will have access to the <br />
+          following features to track your happiness!
         </div>
-        <div className="flex space-x-4 justify-center items-center mt-6  mx-10 rounded-3xl h-16">
+        <div className="flex space-x-6 justify-center items-center mt-6  mx-10 rounded-3xl h-16">
           <div>
-            <input
-              type="text"
-              placeholder="0"
-              className="block w-14 h-14 text-center rounded-2xl bg-white  text-gray-700 py-2  text-xl focus:outline-none focus:border-blue-500"
-            />
+            <div className="flex w-14 h-14 justify-center items-center  bg-[#003437] rounded-2xl">
+              <img src={seek} className="w-8" alt="" />
+            </div>
+            Seek
           </div>
 
           <div>
-            <input
-              type="text"
-              placeholder="0"
-              className="block w-14 h-14 text-center rounded-2xl bg-white  text-gray-700 py-2  text-xl  focus:outline-none focus:border-blue-500"
-            />
+            <div className="flex w-14 h-14 bg-[#003437] rounded-2xl">
+              <img src={journey} alt="" />
+            </div>
+            Journey
           </div>
+
           <div>
-            <input
-              type="text"
-              placeholder="0"
-              className="block w-14 h-14 text-center rounded-2xl bg-white  text-gray-700 py-2  text-xl focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="0"
-              className="block w-14 h-14 text-center rounded-2xl bg-white  text-gray-700 py-2  text-xl  focus:outline-none focus:border-blue-500"
-            />
+            <div className="flex w-14 h-14 justify-center bg-[#003437] rounded-2xl">
+              <img src={story} className="w-10" alt="" />
+            </div>
+            Story
           </div>
         </div>
 
-        <div className="flex justify-center ">
-          <div className="flex justify-center w-20 h-20 bg-yellow-400 mt-64 rounded-full">
+        <div className="justify-center mt-10 font-normal text-white">
+            To unlock true power of Sarathi, Please <br />
+            answer these questions Sarathi has <br />
+            curated for you.
+          <br />
+          <br />
+          It will take just <span className="font-extrabold"> 2min</span> to complete this.
+        </div>
+
+        <div className="flex justify-center">
+          <button className="flex justify-center items-center w-16 h-16 bg-yellow-400 mt-6 rounded-full">
             <img src={sarthilogo} className="w-12 justify-center" alt="" />
-          </div>
+          </button>
         </div>
       </div>
     </div>
